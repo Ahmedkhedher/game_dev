5824963095984e787d1669122fa541294eae6152
Hi 👋, I'm Ahmed Abdilbari khedher <ahmed.kad2003@hotmail.com>
2025-10-05 21:23:32 +0100

    t_version1

--- player.gd
+++ player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const normal_speed = 300.0
+var SPEED = normal_speed
 const JUMP_VELOCITY = -400.0
 
 
+var roll_speed = 600.0
+
+
 func _physics_process(delta: float) -> void:
+	$CanvasLayer/Label.text = "velocity :" + str(abs(velocity.x))
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -18,17 +22,39 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction >0:
-		$AnimatedSprite2D.play("run")
-		$AnimatedSprite2D.flip_h = false
-		
+		if Input.is_action_just_pressed("shift"):
+			$AnimatedSprite2D.play("roll")
+			$animation/roll_timer.start()
+			SPEED =roll_speed
+			SPEED =lerp(roll_speed,normal_speed,0.5)
+		elif $animation/roll_timer.is_stopped():
+			$AnimatedSprite2D.play("run")
+			$AnimatedSprite2D.flip_h = false
+
 	elif direction <0:
-		$AnimatedSprite2D.play("run")
-		$AnimatedSprite2D.flip_h = true
+		if Input.is_action_just_pressed("shift"):
+			$AnimatedSprite2D.play("roll")
+			$animation/roll_timer.start()
+			SPEED =lerp(roll_speed,normal_speed,0.5)
+		elif $animation/roll_timer.is_stopped():
+			$AnimatedSprite2D.play("run")
+			$AnimatedSprite2D.flip_h = true
 	else:
-		$AnimatedSprite2D.play("new_animation_1")
+		if $animation/roll_timer.is_stopped():
+			$AnimatedSprite2D.play("idle")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+
+
+func roll_physics():
+	var jump_distance = 500
+	if Input.is_action_just_pressed("shift"):
+		$AnimatedSprite2D.play("roll")
+
+
+func _on_roll_timer_timeout() -> void:
+	SPEED=normal_speed
