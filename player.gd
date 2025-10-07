3393b47a339648c6d810c7d6bf1183bd450027b2
Hi 👋, I'm Ahmed Abdilbari khedher <ahmed.kad2003@hotmail.com>
2025-10-07 21:11:14 +0100

    tv1.1
    added jump and fall animation

--- player.gd
+++ player.gd
@@ -4,48 +4,67 @@ const normal_speed = 300.0
 var SPEED = normal_speed
 const JUMP_VELOCITY = -400.0
 var roll_speed = 600.0
+var direction
+
+var roll_state = false # check if the player is rolling or not
 
 
 func _physics_process(delta: float) -> void:
-	$CanvasLayer/Label.text = "velocity :" + str(abs(velocity.y))
+	$CanvasLayer/Label.text = "velocity :" + str(abs(velocity.x))
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump and fall
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		print("Current animation:", $AnimatedSprite2D.animation)
+		$animation/roll_timer.start()
+		SPEED = roll_speed
+
+
+
+
+
+
+
+	if roll_state == false:
+		direction = Input.get_axis("ui_left", "ui_right")
+
+
+	if Input.is_action_just_pressed("shift"):
+		$animation/roll_timer.start()
+		$AnimatedSprite2D.play("roll")
+		SPEED = roll_speed
+		roll_state = true
+		$AnimatedSprite2D.offset.y = 0
+	elif velocity.y < 0 and roll_state == false:
 		$AnimatedSprite2D.play("jump")
-		
-	if not is_on_floor() and velocity.y>0:
-			$AnimatedSprite2D.play("fall")
-		
+		$AnimatedSprite2D.offset.y = -20.0
+	elif velocity.y > 0 and roll_state == false:
+		$AnimatedSprite2D.play("fall")
+		$AnimatedSprite2D.offset.y = -20.0
+	elif $animation/roll_timer.is_stopped():
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.offset.y = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+# handels the sprite direction and idle animation
 	if direction >0:
-		if Input.is_action_just_pressed("shift"):
-			$AnimatedSprite2D.play("roll")
-			$animation/roll_timer.start()
-			##print("Current animation:", $AnimatedSprite2D.animation)
-			SPEED = roll_speed
-		elif $animation/roll_timer.is_stopped():
-			$AnimatedSprite2D.play("run")
-			$AnimatedSprite2D.flip_h = false
-
+		$AnimatedSprite2D.flip_h = false
 	elif direction <0:
-		if Input.is_action_just_pressed("shift"):
-			$AnimatedSprite2D.play("roll")
-			$animation/roll_timer.start()
-			SPEED =roll_speed
-		elif $animation/roll_timer.is_stopped():
-			$AnimatedSprite2D.play("run")
-			$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.flip_h = true
 	else:
-		if $animation/roll_timer.is_stopped():
-			$AnimatedSprite2D.play("idle")
+		if velocity.y < 0  and roll_state == false:
+			print("jumping")
+			$AnimatedSprite2D.play("jump")
+			$AnimatedSprite2D.offset.y = -20.0
+		elif velocity.y > 0  and roll_state == false:
+			print("falling")
+			$AnimatedSprite2D.play("fall")
+			$AnimatedSprite2D.offset.y = -20.0
+		else:
+			if $animation/roll_timer.is_stopped():
+				$AnimatedSprite2D.play("idle")
+				$AnimatedSprite2D.offset.y = 0
+				
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -54,15 +73,13 @@ func _physics_process(delta: float) -> void:
 
 
 
-func roll_physics(normal_speed,Speed):
-	while Speed>normal_speed:
-		Speed-=25
-	return Speed
+
 
 
 
 func _on_roll_timer_timeout() -> void:
 	$animation/Timer.start()
+	roll_state = false
 
 
 func _on_timer_timeout() -> void:
