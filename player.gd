05f7fe31f5f8d4490550e8d5bf07d489e605bc32
abdannasser <abdmbarki2002@gmail.com>
2025-10-07 08:55:56 +0100

    ahmed raw logic mta3ik de5il fi 7it XD

--- player.gd
+++ player.gd
@@ -3,20 +3,24 @@ extends CharacterBody2D
 const normal_speed = 300.0
 var SPEED = normal_speed
 const JUMP_VELOCITY = -400.0
-
-
 var roll_speed = 600.0
 
 
 func _physics_process(delta: float) -> void:
-	$CanvasLayer/Label.text = "velocity :" + str(abs(velocity.x))
+	$CanvasLayer/Label.text = "velocity :" + str(abs(velocity.y))
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Handle jump and fall
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		print("Current animation:", $AnimatedSprite2D.animation)
+		$AnimatedSprite2D.play("jump")
+		
+	if not is_on_floor() and velocity.y>0:
+			$AnimatedSprite2D.play("fall")
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,6 +29,7 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("shift"):
 			$AnimatedSprite2D.play("roll")
 			$animation/roll_timer.start()
+			##print("Current animation:", $AnimatedSprite2D.animation)
 			SPEED = roll_speed
 		elif $animation/roll_timer.is_stopped():
 			$AnimatedSprite2D.play("run")
